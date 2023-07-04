a2aa5b96965bd94ef1dad740386c14af5a8e174f
Sandeep Kunichi <138591182+Sandeepkun@users.noreply.github.com>
2023-07-04 10:27:55 -0400

    Create exports.js

---
+++ exports.js
@@ -0,0 +1,6 @@
+module.exports = {
+  apiKey: 'aBcDeFgHiJ1234567890',
+  apiSecret: 'aBcDeFgHiJkLmNoPqRsTuV1234567890',
+  accessToken: '1234567890-aBcDeFgHiJkLmNoPqRsTuV',
+  accessTokenSecret: 'aBcDeFgHiJkLmNoPqRsTuVwXyZ1234567890'
+};
